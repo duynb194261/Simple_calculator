8e61b26f049d54e7b525a9920940d6b927584210
duynb194261 <duy.nb194261@sis.hust.edu.vn>
2021-11-06 23:01:39 +0700

    06/10

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,310 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button but1, but2, but3, but4, but5, but6, but7, but8, but9, but0;
+    private Button butsum, butsub, butmul, butdiv;
+    private Button butC, butCE, butDel, butResult;
+    private TextView Resultview, Inputview;
+    private String input, symbol;
+    boolean checkequal=false;
+    double num1, num2, answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        anhxa();
+
+        butC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Inputview.setText("");
+                Resultview.setText("");
+                input="";
+                symbol="";
+                checkequal=false;
+
+            }
+        });
+
+        but0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input= Resultview.getText().toString();
+                    Resultview.setText(input+"0");
+                }
+            }
+        });
+
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input= Resultview.getText().toString();
+                    Resultview.setText(input+"1");
+                }
+            }
+        });
+
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input= Resultview.getText().toString();
+                    Resultview.setText(input+"2");
+                }
+            }
+        });
+        but3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input= Resultview.getText().toString();
+                    Resultview.setText(input+"3");
+                }
+            }
+        });
+
+        but4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input= Resultview.getText().toString();
+                    Resultview.setText(input+"4");
+                }
+            }
+        });
+
+        but5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input= Resultview.getText().toString();
+                    Resultview.setText(input+"5");
+                }
+            }
+        });
+
+        but6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input= Resultview.getText().toString();
+                    Resultview.setText(input+"6");
+                }
+            }
+        });
+
+        but7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input= Resultview.getText().toString();
+                    Resultview.setText(input+"7");
+                }
+            }
+        });
+
+        but8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input= Resultview.getText().toString();
+                    Resultview.setText(input+"8");
+                }
+            }
+        });
+
+        but9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input= Resultview.getText().toString();
+                    Resultview.setText(input+"9");
+                }
+            }
+        });
+
+
+
+        butsum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkequal||symbol!=""){
+                    solve();
+                    symbol="+";
+                    Inputview.setText(answer+"+");
+                    num1= answer;
+                    checkequal=false;
+                }
+                else if(Resultview.getText()==""){
+                    Inputview.setText("");
+                }
+                else{
+                    num1= Double.parseDouble(Resultview.getText()+"");
+                    Inputview.setText(Resultview.getText().toString()+"+");
+                    Resultview.setText("");
+                    input="";
+                    symbol="+";
+                    checkequal=false;
+                }
+            }
+        });
+
+        butsub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkequal||symbol!=""){
+                    solve();
+                    symbol="-";
+                    Inputview.setText(answer+"-");
+                    num1= answer;
+                    checkequal=false;
+                }
+                else if(Resultview.getText()==""){
+                    Inputview.setText("");
+                }
+                else{
+                    num1= Double.parseDouble(Resultview.getText()+"");
+                    Inputview.setText(Resultview.getText().toString()+"-");
+                    Resultview.setText("");
+                    input="";
+                    symbol="-";
+                    checkequal=false;
+                }
+            }
+        });
+
+        butmul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkequal||symbol!=""){
+                    solve();
+                    symbol="×";
+                    Inputview.setText(answer+"×");
+                    num1= answer;
+                    checkequal=false;
+                }
+                else if(Resultview.getText()==""){
+                    Inputview.setText("");
+                }
+                else{
+                    num1= Double.parseDouble(Resultview.getText()+"");
+                    Inputview.setText(Resultview.getText().toString()+"×");
+                    Resultview.setText("");
+                    input="";
+                    symbol="×";
+                    checkequal=false;
+                }
+            }
+        });
+
+        butdiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkequal||symbol!=""){
+                    solve();
+                    symbol="×";
+                    Inputview.setText(answer+"×");
+                    num1= answer;
+                    checkequal=false;
+                }
+                else if(Resultview.getText()==""){
+                    Inputview.setText("");
+                }
+                else{
+                    num1= Double.parseDouble(Resultview.getText()+"");
+                    Inputview.setText(Resultview.getText().toString()+"÷");
+                    Resultview.setText("");
+                    input="";
+                    symbol="÷";
+                    checkequal=false;
+                }
+            }
+        });
+
+        butResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkequal){
+                    input=Resultview.getText().toString();
+                    num2= Double.parseDouble(Resultview.getText()+"");
+                    Inputview.setText(Inputview.getText().toString()+Resultview.getText().toString()+"");
+                    Resultview.setText("");
+                    if(symbol=="+")
+                        answer=num1+num2;
+                    else if(symbol=="-")
+                        answer=num1-num2;
+                    else if(symbol=="×")
+                        answer=num1*num2;
+                    else if(symbol=="÷")
+                        answer=num1/num2;
+                    checkequal=true;
+                    Resultview.setText(answer+"");
+
+                }
+                symbol="";
+            }
+        });
+
+        butCE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Resultview .setText("");
+            }
+        });
+
+    }
+
+    public void solve(){
+        input=Resultview.getText().toString();
+        num2= Double.parseDouble(Resultview.getText()+"");
+        Resultview.setText("");
+        if(symbol=="+")
+            answer=num1+num2;
+        else if(symbol=="-")
+            answer=num1-num2;
+        else if(symbol=="×")
+            answer=num1*num2;
+        else if(symbol=="÷")
+            answer=num1/num2;
+    }
+
+    public void butdelclick(View view){
+        input=Resultview.getText().toString();
+        if(input.length()>0){
+            String newtext= input.substring(0, input.length()-1);
+            input = newtext;
+        }
+        Resultview.setText(input+"");
+    }
+    public void anhxa(){
+        but0 = findViewById(R.id.butt_0);
+        but1 = findViewById(R.id.butt_1);
+        but2 = findViewById(R.id.butt_2);
+        but3 = findViewById(R.id.butt_3);
+        but4 = findViewById(R.id.butt_4);
+        but5 = findViewById(R.id.butt_5);
+        but6 = findViewById(R.id.butt_6);
+        but7 = findViewById(R.id.butt_7);
+        but8 = findViewById(R.id.butt_8);
+        but9 = findViewById(R.id.butt_9);
+        butsum = findViewById(R.id.butt_add);
+        butsub = findViewById(R.id.butt_sub);
+        butmul = findViewById(R.id.butt_multiply);
+        butdiv = findViewById(R.id.butt_divide);
+        butC = findViewById(R.id.butt_C);
+        butCE = findViewById(R.id.butt_CE);
+        butResult = findViewById(R.id.butt_result);
+        Inputview = findViewById(R.id.text_operation);
+        Resultview = findViewById(R.id.text_result);
+
     }
 }
